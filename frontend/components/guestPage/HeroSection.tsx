95dd64fb91098dac0853d997d99fef37fdd736d6
signor1 <emmanuelomemgboji@gmail.com>
2025-02-13 14:39:35 +0100

    feat: update herosection

--- frontend/components/guestPage/HeroSection.tsx
+++ frontend/components/guestPage/HeroSection.tsx
@@ -1,25 +1,26 @@
 'use client'
 import React from 'react'
 import { DNA } from 'react-loader-spinner'
-import { Button } from '../ui/button'
 import Image from 'next/image'
 import { motion } from "framer-motion";
-import { Button as Spy } from "react-scroll";
+import Link from 'next/link'
+import { Button } from '../ui/button';
 
 const HeroSection = () => {
+
+    const phoneNumber = "254743931827";
+    const message = encodeURIComponent("Hello Maisha Care, I would like to get an AI consultation.");
+    const whatsappLink = `https://wa.me/${phoneNumber}?text=${message}`;
+
     return (
         <main className="w-full lg:min-h-[100dvh] md:h-[600px] h-auto bg-white overflow-x-hidden flex flex-col justify-start items-center relative">
             <section className='lg:w-[55%] w-full px-4 flex flex-col items-center md:pt-16 pt-10'>
-                <h1 className='text-center font-poppins font-bold md:text-5xl text-4xl text-color4/90'>Maisha Care: Empowering You with Decentralized Healthcare Records</h1>
-                <p className='text-center font-DM text-lg font-[500] tracking-wide md:tracking-widest md:mt-1 mt-2 mb-6 text-color4/70 '>Your Health, Your Data, Your Control.
+                <h1 className='text-center font-poppins font-bold md:text-5xl text-4xl text-color4/90'>Revolutionize Your Healthcare Experience</h1>
+                <p className='text-center font-DM text-lg font-[500] tracking-wide md:mt-1 mt-2 mb-6 text-color4/70 '>Complete consultations in under 10 minutes and a secure onchain health profile at your fingertips.
                 </p>
-                <div className="flex justify-center items-center gap-4">
-                    <Button className='bg-color1 w-[130px] h-[40px] text-white hover:bg-color2'>Get Started</Button>
-                    <Spy
-                        to="about"
-                        smooth={true}
-                        spy={true}
-                        duration={700} className='bg-white border w-[130px] h-[40px] border-color1 text-color1 rounded-md hover:bg-color1 hover:text-white'>Learn More</Spy>
+                <div className="flex md:flex-row flex-col justify-center items-center gap-4">
+                    <Link href={whatsappLink} target="_blank" rel="noopener noreferrer" className='bg-color1 px-4 rounded-md flex justify-center items-center h-[40px] text-white hover:bg-color2'>Get AI Consultation Now</Link>
+                    <Button type="button" className='bg-white border px-4 h-[40px] border-color1 text-color1 rounded-md hover:bg-color1 hover:text-white'>Join the E-Doc Waitlist</Button>
                 </div>
                 <div className="md:block hidden">
                     <DNA
