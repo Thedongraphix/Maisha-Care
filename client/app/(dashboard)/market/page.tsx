c362da906afa3659654082018448900fd79cc19e
Wambugu Gichuki <wwambugu62@gmail.com>
2025-02-27 14:28:48 +0300

    node changes

--- client/app/(dashboard)/market/page.tsx
+++
@@ -1,9 +0,0 @@
-import MarketplaceClient from './MarketplaceClient';
-
-export default function MarketPage() {
-  return (
-    <div className="w-full">
-      <MarketplaceClient />
-    </div>
-  );
-}
