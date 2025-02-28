080920573885cdcbb434c2aa48e3e1a22472298c
Thedongraphix <chrisoketchcn@gmail.com>
2025-02-28 12:42:00 +0300

    Adding Navlinks

--- utils/GuestNavLinks.tsx
+++ utils/GuestNavLinks.tsx
@@ -1,23 +1,29 @@
 export type navLinksTypes = {
     name: string,
     to: string,
+    isScroll: boolean,
+    href?: string
 }
 
 export const navlinks: navLinksTypes[] = [
     {
         name: "About",
-        to: "about"
+        to: "about",
+        isScroll: true
     },
     {
         name: "How it Works",
         to: "howItWorks",
+        isScroll: true
     },
     {
         name: "Features",
         to: "features",
+        isScroll: true
     },
     {
         name: "FAQs",
-        to: "faqs"
+        to: "faqs",
+        isScroll: true
     }
 ]
